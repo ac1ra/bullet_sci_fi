104f4015eb7987022cf28509fdee155c6132cc08
ac1ra <ac1ra@ac1ras-iMac.local>
2019-11-26 17:10:33 +0300

    New update. Adding animation

--- bullet_sci_fi/GameElements.swift
+++ bullet_sci_fi/GameElements.swift
@@ -41,4 +41,145 @@ extension GameScene{
         
         return bullet
     }
+    //1 создание кнопки рестара на начальной панели с помощью SpritKit.
+    func createRestartBtn() {
+        restartBtn = SKSpriteNode(imageNamed: "restart")
+        restartBtn.size = CGSize(width: 100, height: 100)
+        restartBtn.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        restartBtn.zPosition = 6
+        restartBtn.setScale(0)
+        self.addChild(restartBtn)
+        restartBtn.run(SKAction.scale(to: 1.0, duration: 0.3))
+    }
+    //2 создание кнопки паузы на начальной панели с помощью SpritKit.
+    func createPauseBtn() {
+        pauseBtn = SKSpriteNode(imageNamed: "pause")
+        pauseBtn.size = CGSize(width: 40, height: 40)
+        pauseBtn.position = CGPoint(x: self.frame.width - 30, y: 30)
+        pauseBtn.zPosition = 6
+        self.addChild(pauseBtn)
+    }
+    //3 создание информации по подбору очков на панели
+    func createScoreLabel() -> SKLabelNode {
+        let scoreLbl = SKLabelNode()
+        scoreLbl.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2 + self.frame.height / 2.6)
+        scoreLbl.text = "\(score)"
+        scoreLbl.zPosition = 5
+        scoreLbl.fontSize = 50
+        scoreLbl.fontName = "Helvetica-Bold"
+        
+        let scoreBg = SKShapeNode()
+        scoreBg.position = CGPoint(x: 0, y: 0)
+        scoreBg.path = CGPath(roundedRect: CGRect(x: CGFloat(-50), y: CGFloat(-30), width: CGFloat(100), height: CGFloat(100)), cornerWidth: 50, cornerHeight: 50, transform: nil)
+        let scoreBgColor = UIColor(red: CGFloat(0.0 / 255.0), green: CGFloat(0.0 / 255.0), blue: CGFloat(0.0 / 255.0), alpha: CGFloat(0.2))
+        scoreBg.strokeColor = UIColor.clear
+        scoreBg.fillColor = scoreBgColor
+        scoreBg.zPosition = -1
+        scoreLbl.addChild(scoreBg)
+        
+        return scoreLbl
+    }
+    //4 создание 2-ой информации для набора очков
+    func createHighscoreLabel() -> SKLabelNode {
+        let highscoreLbl = SKLabelNode()
+        highscoreLbl.position = CGPoint(x: self.frame.width - 80, y: self.frame.height - 22)
+        if let highestScore = UserDefaults.standard.object(forKey: "highestScore") {
+            highscoreLbl.text = "Highest Score: \(highestScore)"
+        } else {
+            highscoreLbl.text = "Highest Score: 0"
+        }
+        highscoreLbl.zPosition = 5
+        highscoreLbl.fontSize = 15
+        highscoreLbl.fontName = "Helvetice-Bold"
+        
+        return highscoreLbl
+    }
+    //5 создание логотипа на панели
+    func createLogo() {
+        logoImg = SKSpriteNode()
+        logoImg = SKSpriteNode(imageNamed: "logo")
+        logoImg.size = CGSize(width: 272, height: 65)
+        logoImg.position = CGPoint(x: self.frame.midX, y: self.frame.midY + 100)
+        logoImg.setScale(0.5)
+        self.addChild(logoImg)
+        logoImg.run(SKAction.scale(to: 1.0, duration: 0.3))
+    }
+    //6
+    func createTaptoplayLabel() -> SKLabelNode {
+        let taptoplayLbl = SKLabelNode()
+        taptoplayLbl.position = CGPoint(x: self.frame.midX, y: self.frame.midY - 100)
+        taptoplayLbl.text = "Tap here"
+        taptoplayLbl.fontColor = UIColor(red: 63/255, green: 79/255, blue: 145/255, alpha: 1.0)
+        taptoplayLbl.zPosition = 5
+        taptoplayLbl.fontSize = 20
+        taptoplayLbl.fontName = "HelveticaNeue"
+        return taptoplayLbl
+    }
+    func createWalls() -> SKNode {
+        //1 Here you instantiate a flower node just like any other node. You set its categoryBitMask to flower and contactBitMask to bird because you need to detect contacts with the bird.
+        
+        let flowerNode = SKSpriteNode(imageNamed: "flower")
+        flowerNode.size = CGSize(width: 40, height: 40)
+        flowerNode.position = CGPoint(x: self.frame.width + 25, y: self.frame.height / 2)
+        
+        flowerNode.physicsBody = SKPhysicsBody(rectangleOf: flowerNode.size)
+        flowerNode.physicsBody?.affectedByGravity = false
+        flowerNode.physicsBody?.isDynamic = false
+        flowerNode.physicsBody?.categoryBitMask = CollisionBitMask.flowerCategory
+        flowerNode.physicsBody?.collisionBitMask = 0
+        flowerNode.physicsBody?.contactTestBitMask = CollisionBitMask.bulletCategory
+        
+        flowerNode.color = SKColor.blue
+        
+        //2 You create an SKNode object named wallPair to add the top and bottom pillars to it as childs. Then you create the top and bottom pillars and set their scale to 0.5. This scales the nodes to half their sizes, setScale is used to increase or decrease the size of the sprite about the scale factor. Since the same images are used for both the top and bottom pillars, you rotate the top pillar node by 180 degrees for the pillar to orient correctly. Pillars are designed to remain stationary so you assign their dynamic property to false.
+        
+        wallPair = SKNode()
+        wallPair.name = "wallPair"
+        
+        let topWall = SKSpriteNode(imageNamed: "pillar")
+        let bttmWall = SKSpriteNode(imageNamed: "pillar")
+        
+        topWall.position = CGPoint(x: self.frame.width + 25, y: self.frame.height / 2 + 420)
+        bttmWall.position = CGPoint(x: self.frame.width + 25, y: self.frame.height / 2 - 420)
+        
+        topWall.setScale(0.5)
+        bttmWall.setScale(0.5)
+        
+        topWall.physicsBody = SKPhysicsBody(rectangleOf: topWall.size)
+        topWall.physicsBody?.categoryBitMask = CollisionBitMask.pillarCategory
+        topWall.physicsBody?.collisionBitMask = CollisionBitMask.bulletCategory
+        topWall.physicsBody?.contactTestBitMask = CollisionBitMask.bulletCategory
+        topWall.physicsBody?.isDynamic = false
+        topWall.physicsBody?.affectedByGravity = false
+        
+        bttmWall.physicsBody = SKPhysicsBody(rectangleOf: bttmWall.size)
+        bttmWall.physicsBody?.categoryBitMask = CollisionBitMask.pillarCategory
+        bttmWall.physicsBody?.collisionBitMask = CollisionBitMask.bulletCategory
+        bttmWall.physicsBody?.contactTestBitMask = CollisionBitMask.bulletCategory
+        bttmWall.physicsBody?.isDynamic = false
+        bttmWall.physicsBody?.affectedByGravity = false
+        
+        topWall.zRotation = CGFloat(M_PI)
+        
+        wallPair.addChild(topWall)
+        wallPair.addChild(bttmWall)
+        
+        wallPair.zPosition = 1
+        
+        //3 This will generate a random number between -200 and 200 using the random() function, and will add that value to the wallPair “y” position. This places the wallPairs at random heights. You then call the moveAndRemove action on wallPair which moves it horizontally and then removes it when it reaches the other side of the screen.
+        
+        let randomPosition = random(min: -200, max: 200)
+        wallPair.position.y = wallPair.position.y + randomPosition
+        wallPair.addChild(flowerNode)
+        
+        wallPair.run(moveAndRemove)
+        
+        return flowerNode
+    }
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random()*(max-min) + min
+    }
 }
--- bullet_sci_fi/GameScene.swift
+++ bullet_sci_fi/GameScene.swift
@@ -20,8 +20,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var scoreLbl = SKLabelNode()
     var highscoreLbl = SKLabelNode()
     var taptoplayLbl = SKLabelNode()
-    var restartBtn = SKLabelNode()
-    var pauseBtn = SKLabelNode()
+    var restartBtn = SKSpriteNode()
+    var pauseBtn = SKSpriteNode()
     var logoImg = SKSpriteNode()
     var wallPair =  SKNode()
     var moveAndRemove = SKAction()
@@ -65,6 +65,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let animateBullet = SKAction.animate(with: self.bulletSprites as! [SKTexture], timePerFrame: 0.1)
         self.repeatActionBullet = SKAction.repeatForever(animateBullet)
         
+        scoreLbl = createScoreLabel()
+        self.addChild(scoreLbl)
+        
+        highscoreLbl = createHighscoreLabel()
+        self.addChild(highscoreLbl)
+        
+        createLogo()
+        
+        taptoplayLbl = createTaptoplayLabel()
+        self.addChild(taptoplayLbl)
     }
     
     override func didMove(to view: SKView) {
@@ -72,6 +82,80 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if isGameStarted == false{
+            //1
+            isGameStarted = true
+            bullet.physicsBody?.affectedByGravity = true
+            createPauseBtn()
+            //2
+            logoImg.run(SKAction.scale(to: 0.5, duration: 0.3),completion: {
+                self.logoImg.removeFromParent()
+            })
+            taptoplayLbl.removeFromParent()
+            //3
+            self.bullet.run(repeatActionBullet)
+            
+            //TODO add pillars here
+            //1 This run an action that creates and add pillar pairs to the scene.
+            
+            let spawn = SKAction.run({
+                () in
+                self.wallPair = self.createWalls()
+                self.addChild(self.wallPair)
+            })
+            
+            //2 Here you wait for 1.5 seconds for the next set of pillars to be generated. A sequence of actions will run the spawn and delay actions forever.
+            
+            let delay = SKAction.wait(forDuration: 1.5)
+            let SpawnDelay = SKAction.sequence([spawn,delay])
+            let spawnDelayForever = SKAction.repeatForever(SpawnDelay)
+            self.run(spawnDelayForever)
+            
+            //3 This will move and remove the pillars. You set the distance that the pillars have to move which is the sum of the screen and the pillar width. Another sequence of action will run in order to move and remove the pillars. Pillars start moving to the left of the screen as they are created and are deallocated when they go off the screen.
+            
+            let distance = CGFloat(self.frame.width + wallPair.frame.width)
+            let movePillars = SKAction.moveBy(x: -distance - 50, y: 0, duration: TimeInterval(0.008 * distance))
+            let removePillars = SKAction.removeFromParent()
+            moveAndRemove = SKAction.sequence([movePillars, removePillars])
+            
+            bullet.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            bullet.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 40))
+            
+        } else {
+            if isDied == false{
+                bullet.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                bullet.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 40))
+            }
+        }
+        for touch in touches{
+            let location = touch.location(in: self)
+            //1
+            if isDied == true{
+                if restartBtn.contains(location){
+                    if UserDefaults.standard.object(forKey: "highestScore") != nil {
+                        let hscore = UserDefaults.standard.integer(forKey: "highestScore")
+                        if hscore < Int(scoreLbl.text!)!{
+                            UserDefaults.standard.set(scoreLbl.text, forKey: "highestScore")
+                        }
+                    } else {
+                        UserDefaults.standard.set(0, forKey: "highestScore")
+                    }
+                    restartScene()
+                }
+            } else {
+                //2
+                if pauseBtn.contains(location){
+                    if self.isPaused == false{
+                        self.isPaused = true
+                        pauseBtn.texture = SKTexture(imageNamed: "play")
+                    } else {
+                        self.isPaused = false
+                        pauseBtn.texture = SKTexture(imageNamed: "pause")
+                    }
+                }
+            }
+        }
         
     }
     override func update(_ currentTime: TimeInterval) {
@@ -88,5 +172,42 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 }))
             }
         }
+    }
+    func didBegin(_ contact: SKPhysicsContact) {
+        let firstBody = contact.bodyA
+        let secondBody = contact.bodyB
+        
+        if firstBody.categoryBitMask == CollisionBitMask.bulletCategory && secondBody.categoryBitMask == CollisionBitMask.pillarCategory || firstBody.categoryBitMask == CollisionBitMask.pillarCategory && secondBody.categoryBitMask == CollisionBitMask.bulletCategory || firstBody.categoryBitMask == CollisionBitMask.bulletCategory && secondBody.categoryBitMask == CollisionBitMask.groundCategory || firstBody.categoryBitMask == CollisionBitMask.groundCategory && secondBody.categoryBitMask == CollisionBitMask.bulletCategory {
+            enumerateChildNodes(withName: "wallPair", using: ({
+                (node, error) in
+                node.speed = 0
+                self.removeAllActions()
+            }))
+            if isDied == false{
+                isDied = true
+                createRestartBtn()
+                pauseBtn.removeFromParent()
+                self.bullet.removeAllActions()
+            }
+        } else if firstBody.categoryBitMask == CollisionBitMask.bulletCategory && secondBody.categoryBitMask == CollisionBitMask.flowerCategory{
+            run(coinSound)
+            score += 1
+            scoreLbl.text = "\(score)"
+            secondBody.node?.removeFromParent()
+        } else if firstBody.categoryBitMask == CollisionBitMask.flowerCategory && secondBody.categoryBitMask == CollisionBitMask.bulletCategory{
+            run(coinSound)
+            score += 1
+            scoreLbl.text = "\(score)"
+            firstBody.node?.removeFromParent()
+        }
+        
+    }
+    func restartScene() {
+        self.removeAllChildren()
+        self.removeAllActions()
+        isDied = false
+        isGameStarted = false
+        score = 0
+        createScene()
     }
 }
